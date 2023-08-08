4af34fd3e93f0fd2dda6d351e8ac9d872ef2d93d
RouatbiZack <rouatbizakaria00@gmail.com>
2023-08-08 09:23:09 +0100

    admin Signin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
-import Home from './Page/Home';
 import Blank from './Page/Blank';
 import Button from './Page/Button';
 import Chart from './Page/Chart';
@@ -8,9 +7,8 @@ import Element from './Page/Element';
 import Error404 from './Page/Error404';
 import Form from './Page/Form';
 import Dashboard from './Page/Dashboard';
-
+import Home from './Page/Home';
 import Signin from './Page/Signin'; 
-import Signup from './Page/Signup';
 import Table from './Page/Table';
 import Typography from './Page/Typography';
 import Widget from './Page/Widget';
@@ -23,8 +21,9 @@ function App() {
       <Router>
          
           <Routes>
-             <Route path="/" element={<Home />} >
-             <Route path="" element={<Dashboard/>} />
+          <Route path="/" element={<Signin />} >
+          
+             <Route path="dashboard" element={<Dashboard/>} />
              <Route path="Element" element={<Element/>} />
              <Route path="Button" element={<Button />} />
              <Route path="Typography" element={< Typography />} />
@@ -32,8 +31,7 @@ function App() {
              <Route path="Form" element={<Form/>} />
              <Route path="Table" element={<Table />} />
              <Route path='Chart' element={<Chart />} />
-             <Route path="Signin" element={< Signin />} />
-             <Route path="Signup" element={<Signup />} />  
+             
              <Route path="Blank" element={<Blank />} />
              <Route path="Error404" element={<Error404/>} />
             </Route>
--- admin/src/Page/Signin.js
+++ admin/src/Page/Signin.js
@@ -1,48 +1,111 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useRef, useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
+
 const Signin = () => {
-  
-  return (
-    <div className="container-fluid position-relative d-flex p-0">
-     <div class="content">
-    <div className="container-fluid position-relative d-flex p-0">
-      <div className="container-fluid">
-        <div className="row h-100 align-items-center justify-content-center" style={{ minHeight: "0vh" }}>
-        <div className="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4" style={{width:"auto"}}>
-            <div className="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
-              <div className="d-flex align-items-center justify-content-between mb-3">
-                <a href="index.html" className="">
-                  <h5 className="text-primary">
-                    <i className="fa fa-user-edit me-2"></i>Gamer's zone
-                  </h5>
-                </a>
-                <h5>Sign In</h5>
-              </div>
-              <div className="form-floating mb-3">
-                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-                <label htmlFor="floatingInput">Email address</label>
-              </div>
-              <div className="form-floating mb-4">
-                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-                <label htmlFor="floatingPassword">Password</label>
-              </div>
-              <div className="d-flex align-items-center justify-content-between mb-4">
-                <div className="form-check">
-                  <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                  <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                </div>
-                <Link  to="">Forgot Password</Link>
-              </div>
-              <button type="submit" className="btn btn-primary py-3 w-100 mb-4">Sign In</button>
-              <p className="text-center mb-0">Don't have an Account? <Link to="">Sign Up</Link></p>
-            </div>
-          </div>
-        </div>
-      </div>
+  const navigate = useNavigate();
+  const [cookies, setCookie] = useCookies([]);
+  const expirationDate = new Date();
+  expirationDate.setTime(expirationDate.getTime() + 3 * 60 * 60 * 1000);
+  const email = useRef();
+  const password = useRef();
+  const [errorMessage, setErrorMessage] = useState('');
+  const [showPassword, setShowPassword] = useState(false); 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  useEffect(() => {
+    if (cookies.session) {
+      navigate(`/dashboard`);
      
+    }
+  }, [cookies.session, cookies.username, navigate]);
+
+  const handleLogin = () => {
+    if (!email.current.value || !password.current.value) {
+      setErrorMessage('All fields are required.');
+      return;
+    }
+  
+    const loginData = {};
+  
+    if (email.current.value.includes('@')) {
+      loginData.email = email.current.value;
+    } else {
+      loginData.username = email.current.value;
+    }
+  
+    loginData.password = password.current.value;
+  
+    axios
+      .post('http://localhost:5000/admin', loginData)
+      .then(function (response) {
+        const { token, user, sessionId } = response.data;
+  
+        if (token && user && sessionId) {
+          setCookie('session', sessionId, { path: '/', expires: expirationDate  });
+          setCookie('Admin', user.username, { path: '/', expires: expirationDate  });
+          setCookie('id', user._id, { path: '/', expires: expirationDate  });
+          alert('Welcome, ' + user.username);
+          navigate(`/dashboard`);
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          const { message } = error.response.data;
+          setErrorMessage(message);
+        } else {
+          setErrorMessage('An error occurred. Please try again later.');
+        }
+      });
+  };
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        // Call the function that handles login
+        handleLogin();
+      }
+    };
+  
+    // Add the event listener to the document
+    document.addEventListener('keydown', handleKeyPress);
+  
+    // Clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+  return (
+<div className="container-fluid position-relative d-flex align-items-center justify-content-center min-vh-100">
+  <div className="bg-secondary rounded p-4 p-sm-5 mx-3" style={{ maxWidth: "450px", width: "80%" }}>
+    <div className="d-flex align-items-center justify-content-between mb-3">
+      <a href="index.html" className="">
+        <h5 className="text-primary">
+          <i className="fa fa-user-edit me-2">Gamer's zone</i>
+        </h5>
+      </a>
+      <h5>Sign In</h5>
     </div>
+    <div className="form-floating mb-3">
+      <input type="email" className="form-control" id="email" placeholder="name@example.com" ref={email} style={{ width: "100%" }} />
+      <label htmlFor="email">Email address</label>
     </div>
+    <div className="form-floating mb-4">
+      <input type="password" className="form-control" id="password" placeholder="Password" ref={password} style={{ width: "100%" }} />
+      <label htmlFor="password">Password</label>
     </div>
+    {errorMessage && <div className="text-danger mb-3">{errorMessage}</div>}
+    <div className="d-flex align-items-center justify-content-between mb-4">
+      <Link to="">Forgot Password</Link>
+    </div>
+    <button type="submit" className="btn btn-primary py-3 w-100 mb-4" onClick={handleLogin}>Sign In</button>
+    <p className="text-center mb-0">Don't have an Account? <Link to="">Sign Up</Link></p>
+  </div>
+</div>
+
+
+  
    
   )
 }
